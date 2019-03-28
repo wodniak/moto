ac75f71f995f0e858342d6dd91c068fc7e678577
GWozniak <wozgrz96@gmail.com>
2019-03-28 10:17:21 +0100

    moto class with data

--- src/scraper.py
+++ src/scraper.py
@@ -3,6 +3,40 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 
+class CMoto(object):
+    """
+    Motorcycle class to keep all data regarding each machine
+    """
+    data = {
+        'brand'    : None,
+        'model'    : None,
+        'desc'     : None,
+        'prodDate' : None,
+        'price'    : None,
+        'motoType' : None,
+        'mileage'   : None,
+        'engineCC' : None
+    }
+
+    def __init__(self, rawData):
+        """
+        Fill all information regarding motorcycle
+
+        @param rawData : xml with all motorcycle parameters 
+        """
+
+        info = rawData.find_all('li', class_='offer-item__params-item')
+        self.data['prodDate'] = info[0].text.strip('\n').strip()
+        self.data['mileage']   = info[1].text.strip('\n').strip('km').strip()
+        self.data['engineCC'] = info[2].text.strip('\n').strip('cm3').strip()
+        self.data['motoType'] = info[3].text.strip('\n')
+    
+        self.data['brand'] = rawData.find('a', class_ = 'offer-title__link').text.strip().split(' ')[0]
+        self.data['model'] = rawData.find('a', class_ = 'offer-title__link').text.strip().split(' ')[1]
+        self.data['desc']  = rawData.find('h3', class_='offer-item__subtitle').text
+        self.data['price'] = rawData.find('span', class_='offer-price__number').text.strip('PLN').strip()
+
+
 def simpleGetUrl(url):
     """
     Attempts to get the content at `url` by making an HTTP GET request.
@@ -33,11 +67,13 @@ def isGoodResponse(resp):
 
 def main():
     url = 'https://www.otomoto.pl/motocykle-i-quady/honda/cm/?search%5Bcountry%5D='
-    rawHtml = simpleGetUrl(url)
+    rawHtml = simpleGetUrl(url)    #get whole page
     if rawHtml is not None:
-        html = BeautifulSoup(rawHtml, 'html.parser')    #get whole page
+        html = BeautifulSoup(rawHtml, 'html.parser')
         for nr, moto in enumerate(html.select('article')):
-            print(nr, moto.find('h3').text)
+            machine = CMoto(moto)
+            print(machine.data)
+
 
 #starting point
 if __name__ == '__main__':
