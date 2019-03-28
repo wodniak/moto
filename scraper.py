7e7953dda8dfd33778a089a0c4f4e927aeecaf3e
GWozniak <wozgrz96@gmail.com>
2019-03-28 14:11:59 +0100

    config json and scrapping class added

--- scraper.py
+++ scraper.py
@@ -3,22 +3,13 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 import json
-
+from time import sleep
 class CMoto(object):
     """
     Motorcycle class to keep all data regarding each machine
     """
 
-    data = {
-        'brand'    : None,
-        'model'    : None,
-        'desc'     : None,
-        'prodDate' : None,
-        'price'    : None,
-        'motoType' : None,
-        'mileage'  : None,
-        'engineCC' : None
-    }
+    numberOfMotors = 0      #moto object counter
 
     def __init__(self, rawData):
         """
@@ -26,17 +17,41 @@ class CMoto(object):
 
         @param rawData : xml with all motorcycle parameters 
         """
+        self.numberOfMotors += 1     #increment for each new object
 
+        self.data = {
+            'brand'    : None,
+            'model'    : None,
+            'desc'     : None,
+            'prodDate' : None,
+            'price'    : None,
+            'motoType' : None,
+            'mileage'  : None,
+            'engineCC' : None
+        }
+ 
         info = rawData.find_all('li', class_='offer-item__params-item')
         self.data['prodDate'] = info[0].text.strip('\n').strip()
-        self.data['mileage']  = info[1].text.strip('\n').strip('km').strip()
-        self.data['engineCC'] = info[2].text.strip('\n').strip('cm3').strip()
-        self.data['motoType'] = info[3].text.strip('\n')
-    
+        self.data['mileage']  = info[1].text.strip('\n').strip('km').strip() 
+
+#@! this solution sucks...
+        #sometimes there is no engineCC given
+        if len(info) == 4:
+            self.data['engineCC'] = info[2].text.strip('\n').strip('cm3').strip()
+            self.data['motoType'] = info[3].text.strip('\n')
+        else:
+            self.data['motoType'] = info[2].text.strip('\n')
+
         self.data['brand'] = rawData.find('a', class_ = 'offer-title__link').text.strip().split(' ')[0]
         self.data['model'] = rawData.find('a', class_ = 'offer-title__link').text.strip().split(' ')[1]
-        self.data['desc']  = rawData.find('h3', class_='offer-item__subtitle').text
+
+#@! this solution sucks...
+        #sometimes there is no description in offert
+        if rawData.find('h3', class_='offer-item__subtitle') is not None:       
+            self.data['desc']  = rawData.find('h3', class_='offer-item__subtitle').text
+
         self.data['price'] = rawData.find('span', class_='offer-price__number').text[:-4].strip()   #get rid of currency
+
 
 class CScraper(object):
     """
@@ -58,22 +73,19 @@ class CScraper(object):
             print('Decoding JSON config file has failed')
 
 
-    def downloadAllPages(self):
+    def downloadAllMotors(self):
         """
-        Download all pages of motorcycles given in config json
-
-        @return rawMotoInfo : list of raw motorcycle information scrapped from web
+        Download all pages of motorcycle types given in config json and use them to create motorcycle objects
+        @return motors : list of initialized motorcycle objects scrapped from web
         """
-        rawMotoInfo = []
-        for url in self._makeUrl():
-            page = self._simpleGetUrl(url)
-            print(url)
-            print(page)
+        motors = []     #list of all motorcycles
+        for url in self._makeUrl():     #for each motorcycle type in config there is 1 url
+            page = self._simpleGetUrl(url)      
             soup = BeautifulSoup(page, 'html.parser')
-            for rawInfo in soup.select('article'):
-                rawMotoInfo.append(rawInfo)
-        
-        return rawMotoInfo
+            for moto in soup.select('article'):
+                motors.append(CMoto(moto))
+
+        return motors
 
 
     def _makeUrl(self):
@@ -84,6 +96,7 @@ class CScraper(object):
         urls = []
         for moto in self.config['vehicle']:
             urls.append(self.mainUrl + moto['brand'] + '/' + moto['model'] + '/')
+
         return urls
 
 
@@ -94,7 +107,13 @@ class CScraper(object):
         text content, otherwise return None.
         """
         try:
-            with closing(get(url, stream=True)) as resp:
+            #without it page responds with None
+            headers = {
+                'User-Agent': 'alamakota', 
+                'From': 'alama@kota.com'
+                }
+
+            with closing(get(url, headers=headers, stream=True)) as resp:
                 if self._isGoodResponse(resp):
                     return resp.content
                 else:
@@ -119,16 +138,12 @@ class CScraper(object):
 
 
 def main():
-    scraper = CScraper('config.json')
-    allRawMotos = scraper.downloadAllPages()
+    scraper = CScraper('config.json')   #parse json
+    motorList = scraper.downloadAllMotors()     #download data and parse it to objects
 
-    listOfMoto = []
-    for moto in allRawMotos:
-        listOfMoto.append(CMoto(moto))
+    for x in range(len(motorList)): #debug print
+        print(motorList[x].data)
 
-    for x in listOfMoto:
-        print(x.data)
-        
 
 
 #starting point
