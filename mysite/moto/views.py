84fdd714dbb4e64ca8c2f7cb617e92c054b280c2
GWozniak <wozgrz96@gmail.com>
2019-05-26 20:23:25 +0200

    generate config.json from web

--- mysite/moto/views.py
+++ mysite/moto/views.py
@@ -1,10 +1,12 @@
 """
     Declared views of application
 """
+import os
+import json
 
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from django.template import loader
+# from django.template import loader
 from django.http import Http404
 
 from .forms import MotoInputForm
@@ -19,11 +21,9 @@ class MotoView(TemplateView):
 
     def get(self, request):
         """
-            Process GET
+            Process GET request
         """
         form = MotoInputForm()
-        # self.latest_moto_list = CMotorcycles.objects.all().order_by('-pub_date')
-
         context = {'form' : form,
                    'moto_to_scrap': self.moto_to_scrap}
         return render(request, self.template_name, context)
@@ -31,29 +31,51 @@ class MotoView(TemplateView):
 
     def post(self, request):
         """
-            Process POST
+            Process POST request
+        """
+        handler = request.POST.get('button')
+        handler = getattr(self, handler, lambda: "Invalid button handler: {}".format(handler))
+        return handler(request)
+
+
+    def add_button(self, request):
+        """
+            Function handler to button add_button
+            Invoked when clicked
         """
         form = MotoInputForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['post']
             if text not in self.moto_to_scrap:
                 self.moto_to_scrap.append(text)
-                
+
         context = {'form' : form,
                    'moto_to_scrap' : self.moto_to_scrap}
 
         return render(request, self.template_name, context)
 
-# def index(request):
-#     """
-#         main page
-#     """
-#     latest_moto_list = CMotorcycles.objects.all().order_by('-pub_date')
-#     template = loader.get_template('moto/index.html')
-#     context = {
-#         'latest_moto_list': latest_moto_list,
-#     }
-#     return HttpResponse(template.render(context, request))
+
+    def submit_button(self, request):
+        """
+            Function handler to button add_button
+            Start scraper.py script
+        """
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path_to_file = os.path.join(dir_path, "script/config.json")
+
+        #create config.json
+        json_dict = {"vehicle" : []}
+        for name in self.moto_to_scrap:
+            if len(name.split()) == 2:
+                brand, model = name.split()
+
+                json_dict['vehicle'].append({"brand" : brand,
+                                             "model" : model})
+            else:
+                json_dict['vehicle'].append({"brand" : name})
+
+        with open(path_to_file, 'w') as config_file:
+            json.dump(json_dict, config_file, indent=4, sort_keys=True)
 
 
 def detail_moto(request, idx):
